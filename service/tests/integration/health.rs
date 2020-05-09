f2bc2f20fcbb9ae6107ff794483fcf7b92e674aa
Graham Cox <graham@grahamcox.co.uk>
2020-05-09 11:51:09 +0100

    Moved some tests around a bit

--- service/tests/integration/health.rs
+++ service/tests/integration/health.rs
@@ -1,12 +1,14 @@
-mod service;
 use actix_web::test;
 use insta::{assert_display_snapshot, assert_json_snapshot};
+use crate::integration::service::TestService;
 
 #[actix_rt::test]
 async fn integration_test_health() {
-    let service = service::TestService::new().await;
+    let service = TestService::new().await;
 
-    let response = service.request(test::TestRequest::get().uri("/health").to_request()).await;
+    let response = service
+        .request(test::TestRequest::get().uri("/health").to_request())
+        .await;
 
     assert_display_snapshot!(response.headers());
     assert_json_snapshot!(response.to_json().unwrap());
