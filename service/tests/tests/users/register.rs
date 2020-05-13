155d86c12496d25da9eafe3fa4c4f2eaefd9b55b
Graham Cox <graham@grahamcox.co.uk>
2020-05-13 16:40:39 +0100

    More tests for registering users with invalid details

--- service/tests/tests/users/register.rs
+++ service/tests/tests/users/register.rs
@@ -1,13 +1,29 @@
 use crate::{service::TestService};
 use insta::{assert_debug_snapshot, assert_json_snapshot};
 use serde_json::json;
+use rstest::rstest;
 
+#[rstest(name, username, display_name, email_address, avatar_url, password,
+    case("no_fields", None, None, None, None, None),
+    case("all_blank", Some(""), Some(""), Some(""), Some(""), Some("")),
+    case("all_whitespace", Some(" "), Some(" "), Some(" "), Some(" "), Some(" ")),
+    case("no_username", None, Some("Display Name"), Some("test@example.com"), Some("http://example.com"), Some("Password")),
+    case("no_email", Some("username"), Some("Display Name"), None, Some("http://example.com"), Some("Password")),
+    case("no_password", Some("username"), Some("Display Name"), Some("test@example.com"), Some("http://example.com"), None),
+)]
 #[actix_rt::test]
-async fn integration_test_register_no_fields() {
+async fn integration_test_register_validation_failure(name: &str, username: Option<&str>, display_name: Option<&str>, email_address: Option<&str>, avatar_url: Option<&str>, password: Option<&str>) {
     let service = TestService::new().await;
 
-    let response = service.request(actix_web::test::TestRequest::post().uri("/users").set_json(&json!({})).to_request()).await;
+    let body = json!({
+        "username": username,
+        "display_name": display_name,
+        "email_address": email_address,
+        "avatar_url": avatar_url,
+        "password": password
+    });
+    let response = service.request(actix_web::test::TestRequest::post().uri("/users").set_json(&body).to_request()).await;
 
-    assert_debug_snapshot!("register-no_fields-headers", response.headers());
-    assert_json_snapshot!("register-no_fields-body", response.to_json().unwrap());
+    assert_debug_snapshot!(format!("register-{}-headers", name), response.headers());
+    assert_json_snapshot!(format!("register-{}-body", name), response.to_json().unwrap());
 }
