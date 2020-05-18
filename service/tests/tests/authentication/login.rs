bcec20bb4d40ed050691017c659509ba6e9e3c4e
Graham Cox <graham@grahamcox.co.uk>
2020-05-18 15:53:49 +0100

    Initial integration tests for logging in

---
+++ service/tests/tests/authentication/login.rs
@@ -0,0 +1,62 @@
+use crate::{
+    data::SeedUser,
+    service::{TestResponse, TestService},
+};
+use insta::{assert_debug_snapshot, assert_json_snapshot};
+use serde_json::json;
+
+#[test]
+fn integration_test_login_unknown_username() {
+    let service = TestService::new();
+
+    let body = serde_json::to_string(&json!({
+        "username": "username",
+        "password": "password"
+    }))
+    .unwrap();
+    let client = service.test_client();
+    let mut response: TestResponse = client.post("/login").body(&body).dispatch().into();
+
+    assert_debug_snapshot!("login_unknown_username-headers", response.headers());
+    assert_json_snapshot!("login_unknown_username-body", response.to_json().unwrap());
+}
+
+#[test]
+fn integration_test_login_incorrect_password() {
+    let service = TestService::new();
+
+    service.seed(SeedUser {
+        username: "username".to_owned(),
+        ..Default::default()
+    });
+
+    let body = serde_json::to_string(&json!({
+        "username": "username",
+        "password": "password"
+    }))
+    .unwrap();
+    let client = service.test_client();
+    let mut response: TestResponse = client.post("/login").body(&body).dispatch().into();
+
+    assert_debug_snapshot!("login_incorrect_password-headers", response.headers());
+    assert_json_snapshot!("login_incorrect_password-body", response.to_json().unwrap());
+}
+
+#[test]
+fn integration_test_login_success() {
+    let service = TestService::new();
+
+    let body = serde_json::to_string(&json!({
+        "username": "username",
+        "password": "password"
+    }))
+    .unwrap();
+    let client = service.test_client();
+    let mut response: TestResponse = client.post("/login").body(&body).dispatch().into();
+
+    assert_debug_snapshot!("login_success-headers", response.headers());
+    assert_json_snapshot!("login_success-body", response.to_json().unwrap(), {
+        ".token.token" => "[access_token]",
+        ".token.valid_until" => "[access_token_expiry]"
+    });
+}
