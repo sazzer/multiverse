210cc234ebbb427e1523a6c6e6095fc242aea32a
Graham Cox <graham@grahamcox.co.uk>
2020-05-24 11:28:14 +0100

    Support for distinguishing between Missing and Null in a Patch

---
+++ service/src/http/patch.rs
@@ -0,0 +1,214 @@
+use serde::{Deserialize, Deserializer};
+
+/// Enumeration of possible states for each field in an HTTP Patch
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum Patch<T> {
+    /// The field was missing - i.e. it was undefined
+    Missing,
+    // The field was present but didn't have a value
+    Null,
+    /// The field was present and had a value
+    Value(T),
+}
+
+impl<T> Patch<T> {
+    /// Map the value inside the Patch to a different value, possibly of a different type.
+    ///
+    /// A `Missing` or `Null` value is left as-is.
+    /// A `Value(v)` will get transformed into the new value
+    ///
+    /// # Parameters
+    /// - `f` - The function to transform the value
+    ///
+    /// # Returns
+    /// The transformed value
+    pub fn map<U, F: FnOnce(T) -> U>(self, f: F) -> Patch<U> {
+        match self {
+            Patch::Missing => Patch::Missing,
+            Patch::Null => Patch::Null,
+            Patch::Value(v) => Patch::Value(f(v)),
+        }
+    }
+
+    /// Filter the value inside the Patch, returning `Null` if the value fails the predicate
+    ///
+    /// A `Missing` or `Null` value is left as-is.
+    /// A `Value(v)` will get transformed into the new value
+    ///
+    /// # Parameters
+    /// - `predicate` - The function to evaluate the value
+    ///
+    /// # Returns
+    /// The filtered value
+    pub fn filter_null<P: FnOnce(&T) -> bool>(self, predicate: P) -> Self {
+        match self {
+            Patch::Missing => Patch::Missing,
+            Patch::Null => Patch::Null,
+            Patch::Value(v) => {
+                if predicate(&v) {
+                    Patch::Value(v)
+                } else {
+                    Patch::Null
+                }
+            }
+        }
+    }
+
+    /// Filter the value inside the Patch, returning `Missing` if the value fails the predicate
+    ///
+    /// A `Missing` or `Null` value is left as-is.
+    /// A `Value(v)` will get transformed into the new value
+    ///
+    /// # Parameters
+    /// - `predicate` - The function to evaluate the value
+    ///
+    /// # Returns
+    /// The filtered value
+    pub fn filter_missing<P: FnOnce(&T) -> bool>(self, predicate: P) -> Self {
+        match self {
+            Patch::Missing => Patch::Missing,
+            Patch::Null => Patch::Null,
+            Patch::Value(v) => {
+                if predicate(&v) {
+                    Patch::Value(v)
+                } else {
+                    Patch::Missing
+                }
+            }
+        }
+    }
+
+    /// Convert the value to an `Option<T>`.
+    ///
+    /// A `Missing` or `Null` returns `None`.
+    /// A `Value(v)` returns `Some(v)`
+    ///
+    /// # Returns
+    /// The value as an Option
+    pub fn value(self) -> Option<T> {
+        if let Patch::Value(v) = self {
+            Some(v)
+        } else {
+            None
+        }
+    }
+
+    /// Convert the value to an `Option<T>` but wrapped in a `Result`.
+    /// The `Result` returns `Err(err)` if the `Patch` is `Null`, otherwise returns an `Ok(Option<T>)`
+    /// containing the value.
+    ///
+    /// # Parameters
+    /// - `err` - The error value to use
+    ///
+    /// # Returns
+    /// - `Patch::Value(v)` -> `Ok(Some(v))`
+    /// - `Patch::Missing` -> `Ok(None`
+    /// - `Patch::Null` -> `Err(err)`
+    pub fn ok_not_null<E>(self, err: E) -> Result<Option<T>, E> {
+        match self {
+            Patch::Null => Err(err),
+            Patch::Missing => Ok(None),
+            Patch::Value(v) => Ok(Some(v)),
+        }
+    }
+}
+
+impl<T, E> Patch<Result<T, E>> {
+    /// Transpose a patch containing a result into a result containing the patch
+    ///
+    /// # Returns
+    /// - `Patch::Missing` -> Ok(Missing)
+    /// - `Patch::Null` -> Ok(Null)
+    /// - `Patch::Value(Ok(v)) -> Ok(Value(v))
+    /// - `Patch::Value(Err(e)) -> Err(e)
+    pub fn transpose(self) -> Result<Patch<T>, E> {
+        match self {
+            Patch::Missing => Ok(Patch::Missing),
+            Patch::Null => Ok(Patch::Null),
+            Patch::Value(Ok(v)) => Ok(Patch::Value(v)),
+            Patch::Value(Err(e)) => Err(e),
+        }
+    }
+}
+
+impl<T> Default for Patch<T> {
+    fn default() -> Self {
+        Patch::Missing
+    }
+}
+
+impl<T> From<Option<T>> for Patch<T> {
+    fn from(opt: Option<T>) -> Patch<T> {
+        match opt {
+            Some(v) => Patch::Value(v),
+            None => Patch::Null,
+        }
+    }
+}
+
+impl<'de, T> Deserialize<'de> for Patch<T>
+where
+    T: Deserialize<'de>,
+{
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        Option::deserialize(deserializer).map(Into::into)
+    }
+}
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use galvanic_assert::{assert_that, matchers::*};
+
+    #[test]
+    fn test_map() {
+        assert_that!(&Patch::Missing.map(|v: u32| v), eq(Patch::Missing));
+        assert_that!(&Patch::Null.map(|v: u32| v), eq(Patch::Null));
+        assert_that!(&Patch::Value(5).map(|v| v * 2), eq(Patch::Value(10)));
+        assert_that!(&Patch::Value(5).map(|v| format!("{}", v)), eq(Patch::Value("5".to_owned())));
+    }
+
+    #[test]
+    fn test_filter_null() {
+        assert_that!(&Patch::Missing.filter_null(|_: &u32| true), eq(Patch::Missing));
+        assert_that!(&Patch::Missing.filter_null(|_: &u32| false), eq(Patch::Missing));
+        assert_that!(&Patch::Null.filter_null(|_: &u32| true), eq(Patch::Null));
+        assert_that!(&Patch::Null.filter_null(|_: &u32| false), eq(Patch::Null));
+        assert_that!(&Patch::Value(1).filter_null(|_| true), eq(Patch::Value(1)));
+        assert_that!(&Patch::Value(1).filter_null(|_| false), eq(Patch::Null));
+    }
+
+    #[test]
+    fn test_filter_missing() {
+        assert_that!(&Patch::Missing.filter_missing(|_: &u32| true), eq(Patch::Missing));
+        assert_that!(&Patch::Missing.filter_missing(|_: &u32| false), eq(Patch::Missing));
+        assert_that!(&Patch::Null.filter_missing(|_: &u32| true), eq(Patch::Null));
+        assert_that!(&Patch::Null.filter_missing(|_: &u32| false), eq(Patch::Null));
+        assert_that!(&Patch::Value(1).filter_missing(|_| true), eq(Patch::Value(1)));
+        assert_that!(&Patch::Value(1).filter_missing(|_| false), eq(Patch::Missing));
+    }
+
+    #[test]
+    fn test_value() {
+        assert_that!(&Patch::<u32>::Missing.value(), eq(None));
+        assert_that!(&Patch::<u32>::Null.value(), eq(None));
+        assert_that!(&Patch::Value(1).value(), eq(Some(1)));
+    }
+
+    #[test]
+    fn test_ok_not_null() {
+        assert_that!(&Patch::<u32>::Missing.ok_not_null(5), eq(Ok(None)));
+        assert_that!(&Patch::<u32>::Null.ok_not_null(5), eq(Err(5)));
+        assert_that!(&Patch::<u32>::Value(1).ok_not_null(5), eq(Ok(Some(1))));
+    }
+
+    #[test]
+    fn test_transpose() {
+        assert_that!(&Patch::<Result<u32, u32>>::Missing.transpose(), eq(Ok(Patch::Missing)));
+        assert_that!(&Patch::<Result<u32, u32>>::Null.transpose(), eq(Ok(Patch::Null)));
+        assert_that!(&Patch::<Result<u32, u32>>::Value(Ok(1)).transpose(), eq(Ok(Patch::Value(1))));
+        assert_that!(&Patch::<Result<u32, u32>>::Value(Err(5)).transpose(), eq(Err(5)));
+    }
+}
--- service/src/users/endpoints/patch_user.rs
+++ service/src/users/endpoints/patch_user.rs
@@ -1,6 +1,9 @@
 use super::{errors::UserProblemType, model::UserResponse};
 use crate::authorization::Authorizer;
-use crate::http::problem::{GenericValidation, Problem, ValidationProblem};
+use crate::http::{
+    patch::Patch,
+    problem::{GenericValidation, Problem, ValidationProblem},
+};
 use crate::users::*;
 use rocket::{http::Status, patch, State};
 use rocket_contrib::json::Json;
@@ -25,35 +28,56 @@ pub fn patch_user(
 ) -> Result<UserResponse, Problem> {
     authorizer.authorize().same_user(&id).finish()?;
 
-    let display_name = body.display_name();
-    let email_address = body.email_address();
-    let avatar_url = body.avatar_url();
-    let password = body.password();
+    // Display Name: Missing = No Change, Null = Validation Error, Value = Update
+    let display_name = body.display_name().ok_not_null(GenericValidation::Missing);
 
-    if let (Ok(email_address), Ok(password)) = (&email_address, &password) {
+    // Email Address: Missing = No Change, Null = Validation Error, Value = Update
+    let email_address = body
+        .email_address()
+        .map_err(|_| GenericValidation::Missing)
+        .and_then(|email| email.ok_not_null(GenericValidation::Missing));
+
+    // Avatar URL: Missing = No Change, Null = Clear, Value = Update
+    let avatar_url = body.avatar_url();
+
+    // Password: Missing = No Change, Null = Validation Error, Value = Update
+    let password = body
+        .password()
+        .map_err(|_| GenericValidation::Missing)
+        .and_then(|password| password.ok_not_null(GenericValidation::Missing));
+
+    if let (Ok(display_name), Ok(email_address), Ok(password)) =
+        (&display_name, &email_address, &password)
+    {
+        tracing::info!("Updating user");
+
         users_service
             .update_user(&id, &move |user| UserData {
                 display_name: display_name.clone().unwrap_or(user.display_name),
                 email_address: email_address.clone().unwrap_or(user.email_address),
-                avatar_url: avatar_url.clone().or(user.avatar_url),
+                avatar_url: match avatar_url.clone() {
+                    Patch::Value(v) => Some(v),
+                    Patch::Null => None,
+                    Patch::Missing => user.avatar_url,
+                },
                 password: password.clone().unwrap_or(user.password),
                 ..user
             })
             .ok_or_else(|| Problem::new(UserProblemType::UnknownUserID, Status::NotFound))
             .map(|user| user.into())
     } else {
-        tracing::warn!("Validation error registering user");
+        tracing::warn!("Validation error updating user");
 
         let mut problem = ValidationProblem::new();
 
-        if let Err(err) = email_address.map_err(|e| match e {
-            EmailAddressParseError::Blank => GenericValidation::Missing,
-        }) {
+        if let Err(err) = display_name {
+            problem.with_field_error("display_name", err);
+        }
+        if let Err(err) = email_address {
             problem.with_field_error("email_address", err);
         }
-
-        if password.is_err() {
-            problem.with_field_error("password", GenericValidation::Missing);
+        if let Err(err) = password {
+            problem.with_field_error("password", err);
         }
 
         Err(problem.build())
@@ -61,39 +85,42 @@ pub fn patch_user(
 }
 
 /// Incoming details representing a registration request
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Default)]
+#[serde(default)]
 pub struct PatchUserRequest {
     /// The new display name to use
-    display_name: Option<String>,
+    display_name: Patch<String>,
     /// The new email address to use
-    email_address: Option<String>,
+    email_address: Patch<String>,
     /// The new avatar URL to use
-    avatar_url: Option<String>,
+    avatar_url: Patch<String>,
     /// The new password to use
-    password: Option<Plaintext>,
+    password: Patch<Plaintext>,
 }
 
 impl PatchUserRequest {
     /// Extract the display name to use
-    fn display_name(&self) -> Option<String> {
-        self.display_name.clone().filter(|v| !v.trim().is_empty())
+    fn display_name(&self) -> Patch<String> {
+        self.display_name
+            .clone()
+            .filter_null(|v| !v.trim().is_empty())
     }
 
     /// Extract the email address to use
-    fn email_address(&self) -> Result<Option<EmailAddress>, EmailAddressParseError> {
+    fn email_address(&self) -> Result<Patch<EmailAddress>, EmailAddressParseError> {
         self.email_address.clone().map(|v| v.parse()).transpose()
     }
 
     /// Extract the avatar URL to use
-    fn avatar_url(&self) -> Option<String> {
+    fn avatar_url(&self) -> Patch<String> {
         self.avatar_url
             .clone()
-            .filter(|v| !v.trim().is_empty())
+            .filter_null(|v| !v.trim().is_empty())
             .map(|v| v.to_owned())
     }
 
     /// Extract the password to use
-    fn password(&self) -> Result<Option<Password>, PasswordHashError> {
+    fn password(&self) -> Result<Patch<Password>, PasswordHashError> {
         self.password
             .clone()
             .map(Password::from_plaintext)
