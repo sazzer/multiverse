853870e095dc80347a9a6cdcd5606ffa2a64f878
Graham Cox <graham@grahamcox.co.uk>
2020-05-28 20:55:29 +0100

    Added in React Router

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,5 +1,24 @@
+import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
 import React from "react";
 
 export default () => {
-  return <div className="App">Multiverse</div>;
+  return (
+    <Router>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/users">Users</Link>
+      <Switch>
+        <Route path="/about">
+          <div>About</div>
+        </Route>
+        <Route path="/users">
+          <div>Users</div>
+        </Route>
+        <Route path="/">
+          <div>Home</div>
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
