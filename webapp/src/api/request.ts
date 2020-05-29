a8739287aaad4dd3c56f5b3eb04ec28c4a2fde3c
Graham Cox <graham@grahamcox.co.uk>
2020-05-29 17:20:18 +0100

    Support for making HTTP Requests to the server

---
+++ webapp/src/api/request.ts
@@ -0,0 +1,75 @@
+import { Problem } from "./problem";
+import UrlTemplate from "url-template";
+import debug from "debug";
+
+/** The logger to use */
+const LOGGER = debug("multiverse:api:request");
+
+/** The base URL to call */
+const URL_BASE = "https://multiverse-cd.herokuapp.com";
+
+/**
+ * The details needed in order to make an HTTP Request
+ */
+export interface Request {
+  /** The HTTP Method to use. Defaults to GET if not provided */
+  method?: "GET" | "POST" | "PUT" | "DELETE" | "PATCH" | "HEAD";
+  /** Any parameters to use for URL expansion */
+  urlParams?: { [key: string]: any };
+}
+
+/**
+ * The details of an HTTP Response
+ */
+export interface Response<B> {
+  status: number;
+  headers: Headers;
+  body?: B;
+}
+
+/**
+ * Actually make an HTTP request and get the response
+ * @param url The URL to call. This is a URL-Template as defined in RFC-6570.
+ * @param request Any additional parameters for the request
+ */
+export async function request<B>(
+  url: string,
+  request: Request = {}
+): Promise<Response<B>> {
+  const template = UrlTemplate.parse(url);
+  const finalUrl = template.expand(request.urlParams);
+  LOGGER("Making request to %s: %o", finalUrl, request);
+
+  try {
+    const response = await fetch(URL_BASE + finalUrl, {
+      method: request.method || "GET",
+    });
+    LOGGER("Received response from %s: %o", finalUrl, response);
+
+    const contentType = response.headers.get("content-type");
+
+    if (contentType) {
+      const body = await response.json();
+      LOGGER("Response had payload: %o", body);
+
+      if (contentType === "application/problem+json") {
+        LOGGER("Response was a Problem");
+        throw new Problem(
+          body.type as string,
+          body.title as string,
+          response.status,
+          body
+        );
+      } else {
+        LOGGER("Response was not a Problem");
+        return { status: response.status, headers: response.headers, body };
+      }
+    } else {
+      LOGGER("Response had no payload");
+      return { status: response.status, headers: response.headers };
+    }
+  } catch (e) {
+    LOGGER("Unexpected error making HTTP request: %o", e);
+    throw e;
+  }
+}
--- webapp/src/landing/authentication/start.tsx
+++ webapp/src/landing/authentication/start.tsx
@@ -1,12 +1,35 @@
+import { Problem, request } from "../../api";
+
 import React from "react";
 import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 export default () => {
   const { t } = useTranslation();
-  const { register, handleSubmit } = useForm();
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const { register, handleSubmit } = useForm({
+    defaultValues: {
+      username: "",
+    },
+  });
+  const onSubmit = ({ username }: { username: string }) => {
+    request("/usernames/{username}", {
+      urlParams: {
+        username,
+      },
+    })
+      .then((response) => {
+        console.log("Username exists", response);
+      })
+      .catch((e) => {
+        if (
+          e instanceof Problem &&
+          e.type === "tag:multiverse,2020:users/problems/unknown_username"
+        ) {
+          console.log("Username doesn't exist");
+        } else {
+          console.log("Something went wrong", e);
+        }
+      });
   };
 
   return (
