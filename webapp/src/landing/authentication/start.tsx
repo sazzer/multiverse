dfa2a4495ed8dc8bd72a496ad6f6f9d01c11b902
Graham Cox <graham@grahamcox.co.uk>
2020-05-29 13:20:07 +0100

    Added in react-hook-form as a way to manage form state

--- webapp/src/landing/authentication/start.tsx
+++ webapp/src/landing/authentication/start.tsx
@@ -1,16 +1,29 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 import { useTranslation } from "react-i18next";
 
 export default () => {
   const { t } = useTranslation();
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data: any) => {
+    console.log(data);
+  };
 
   return (
-    <form>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <h2>{t("authentication.start.title")}</h2>
 
       <div className="form-group">
         <label htmlFor="username">{t("authentication.username.label")}</label>
-        <input type="text" className="form-control" id="username" autoFocus />
+        <input
+          type="text"
+          className="form-control"
+          id="username"
+          name="username"
+          ref={register({ required: true })}
+          required
+          autoFocus
+        />
       </div>
 
       <button type="submit" className="btn btn-primary">
