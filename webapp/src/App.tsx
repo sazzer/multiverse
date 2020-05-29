faa2759c019a57dc07e1ce71ead12a5dc5ce28a9
Graham Cox <graham@grahamcox.co.uk>
2020-05-29 07:29:09 +0100

    Landing page

--- webapp/src/App.tsx
+++ webapp/src/App.tsx
@@ -1,30 +1,21 @@
-import { Link, Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 
 import HeaderBar from "./header";
+import LandingPage from "./landing";
 import React from "react";
-import { useTranslation } from "react-i18next";
 
 export default () => {
-  const { t } = useTranslation();
-
   return (
     <Router>
       <HeaderBar />
-      <div>{t("page.title")}</div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/users">Users</Link>
-      <Switch>
-        <Route path="/about">
-          <div>About</div>
-        </Route>
-        <Route path="/users">
-          <div>Users</div>
-        </Route>
-        <Route path="/">
-          <div>Home</div>
-        </Route>
-      </Switch>
+
+      <div className="container-fluid">
+        <Switch>
+          <Route path="/">
+            <LandingPage />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 };
---
+++ webapp/src/landing/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import landingPicture from "./landing.jpg";
+
+export default () => {
+  return (
+    <div className="row">
+      <div className="col-12 col-md-3 order-sm-3">Login Area</div>
+      <div className="col-12 col-md-9">
+        <h2>Avaelia</h2>
+        <img
+          src={landingPicture}
+          alt="Avaelia"
+          className="img-fluid img-thumbnail rounded shadow"
+        />
+      </div>
+    </div>
+  );
+};
