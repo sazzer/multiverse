6ce7ab80c4d52746f5d3f154f1aa6d968eef155d
Graham Cox <graham@grahamcox.co.uk>
2020-06-25 08:20:46 +0100

    Able to parse a UserLink from the querystring

--- service/src/users/endpoints/model/user_link.rs
+++ service/src/users/endpoints/model/user_link.rs
@@ -1,6 +1,10 @@
 use crate::users::UserID;
+use rocket::{http::RawStr, request::FromFormValue};
+use std::ops::Deref;
+use uuid::Uuid;
 
 /// Representation of a link to a user
+#[derive(Debug)]
 pub struct UserLink(UserID);
 
 impl UserLink {
@@ -12,5 +16,35 @@ impl UserLink {
 impl From<UserLink> for String {
     fn from(user_link: UserLink) -> String {
         format!("/users/{}", user_link.0)
+    }
+}
+
+impl Deref for UserLink {
+    type Target = UserID;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl<'v> FromFormValue<'v> for UserLink {
+    type Error = &'v RawStr;
+
+    fn from_form_value(form_value: &'v RawStr) -> Result<Self, Self::Error> {
+        form_value
+            .percent_decode()
+            .map_err(|e| {
+                tracing::warn!(e = ?e, value = ?form_value, "Failed to decode parameter");
+                form_value
+            })
+            .map(|param| param.trim_start_matches("/users/").to_owned())
+            .and_then(|user_id| {
+                Uuid::parse_str(&user_id).map_err(|e| {
+                    tracing::warn!(e = ?e, value = ?form_value, "Failed to parse User ID");
+                    form_value
+                })
+            })
+            .map(|user_id| UserID::new(user_id))
+            .map(|user_id| UserLink(user_id))
     }
 }
--- service/src/worlds/endpoints/search_worlds.rs
+++ service/src/worlds/endpoints/search_worlds.rs
@@ -1,6 +1,7 @@
 use crate::{
     http::{pagination::PaginationRequest, problem::Problem, sorts::SortFieldsRequest},
     model::ParseSortFieldError,
+    users::endpoints::model::UserLink,
     worlds::{endpoints::model::WorldResponse, WorldSortField, WorldsService},
 };
 use rocket::{get, State};
@@ -13,9 +14,10 @@ use rocket::{get, State};
 /// # Returns
 /// The details of the worlds that matched the search
 #[tracing::instrument(name = "GET /worlds", skip(worlds_service))]
-#[get("/worlds?<sort>&<pagination..>")]
+#[get("/worlds?<owner>&<sort>&<pagination..>")]
 pub fn search_worlds(
     worlds_service: State<WorldsService>,
+    owner: Option<UserLink>,
     sort: Option<Result<SortFieldsRequest<WorldSortField>, ParseSortFieldError>>,
     pagination: PaginationRequest,
 ) -> Result<WorldResponse, Problem> {
